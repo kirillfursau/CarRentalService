562d6ce0b9e9b0ffb00fc5383863ebcab1b26fb8
kirillfursau <krill19988@gmail.com>
2020-04-29 22:12:53 +0300

    refactor

--- database/startdata.sql
+++ database/startdata.sql
@@ -1,53 +1,50 @@
 create schema car_rental_service;
 USE car_rental_service;
 
+
 create table customer
 (
-    id            bigint      not null,
-    first_name    varchar(25) not null,
-    second_name   varchar(25) not null,
-    date_of_birth date        not null,
-    sex           int         not null comment '0 — undefined
-1 — male
-2 — female',
-    phone_number  int         not null,
-    country       varchar(30) not null,
-    city          varchar(30) not null,
-    street        varchar(30) not null,
-    building      varchar(5)  not null,
-    apartment     varchar(3)  null,
-    passport_id   varchar(25) not null,
-    constraint customer_pk
+    id           bigint      not null,
+    phone_number int         not null,
+    password     varchar(35) not null,
+    constraint car_class_pk
         primary key (id)
 );
 
-
-create table passport
+create table customer_details
 (
-    passport_id          varchar(25) not null,
-    date_of_issue        date        not null,
-    expiry_date          date        not null,
-    place_of_birthday    varchar(30) not null,
-    country_of_residence varchar(30) not null
+    id                     bigint      not null,
+    first_name             varchar(25) not null,
+    second_name            varchar(25) not null,
+    date_of_birth          date        not null,
+    sex                    varchar(10) not null,
+    phone_number_id        bigint      not null,
+    country                varchar(30) not null,
+    city                   varchar(30) not null,
+    street                 varchar(30) not null,
+    building               varchar(5)  not null,
+    apartment              varchar(3)  null,
+    passport_id            varchar(25) not null,
+    passport_date_of_issue date        not null,
+    passport_expiry_date   date        not null,
+    place_of_birthday      varchar(30) not null,
+    country_of_residence   varchar(30) not null,
+    constraint car_class_pk
+        primary key (id),
+    foreign key (phone_number_id) references customer (id)
 );
 
-create unique index passport_passport_id_uindex
-    on passport (passport_id);
-
-alter table customer
-    add constraint customer_fk
-        foreign key (passport_id) references passport (passport_id);
 
 create table car_class
 (
     class             varchar(15) not null,
-    transmission_type boolean     null comment 'true - automatic
-false - manual',
-    air_conditioning  boolean     null,
+    transmission_type varchar(15) not null,
+    air_conditioning  boolean     not null,
     constraint car_class_pk
         primary key (class)
 
 );
+
 create table car
 (
     id                    bigint      not null,
@@ -62,12 +59,10 @@ create table car
     rental_day_price      int         not null,
     car_class             varchar(15) not null,
     constraint car_pk
-        primary key (id)
+        primary key (id),
+    foreign key (car_class) references car_class (class)
 
 );
-alter table car
-    add constraint car_class_fk
-        foreign key (car_class) references car_class (class);
 
 create unique index car_body_number_uindex
     on car (body_number);
@@ -75,28 +70,9 @@ create unique index car_body_number_uindex
 create unique index car_registered_car_number_uindex
     on car (registered_car_number);
 
-create table rental_information
-(
-    id            bigint not null,
-    date_of_issue date   not null,
-    rental_time   int    not null,
-    return_date   date   not null,
-    car_id        bigint not null,
-    customer_id   bigint not null,
-    rental_price  int    not null,
-    constraint rental_information_pk
-        primary key (id)
-);
-alter table rental_information
-    add constraint rental_information_customer_fk
-        foreign key (customer_id) references customer (id);
-alter table rental_information
-    add constraint rental_information_car_fk
-        foreign key (car_id) references car (id);
-
 create table `pick-up_location`
 (
-    id       bigint         not null,
+    id       bigint      not null,
     country  varchar(30) not null,
     city     varchar(30) not null,
     street   varchar(30) not null,
@@ -105,20 +81,34 @@ create table `pick-up_location`
         primary key (id)
 );
 
+create table rental_information
+(
+    id                  bigint not null,
+    date_of_issue       date   not null,
+    rental_time         int    not null,
+    return_date         date   not null,
+    car_id              bigint not null,
+    customer_id         bigint not null,
+    rental_price        int    not null,
+    pick_up_location_id bigint not null,
+    constraint rental_information_pk
+        primary key (id),
+    foreign key (customer_id) references customer (id),
+    foreign key (car_id) references car (id),
+    foreign key (pick_up_location_id) references `pick-up_location` (id)
+);
+
+
 create table `car_pick-up_location`
 (
     id                 bigint not null,
-    car                bigint not null,
-    `pick-up_location` bigint    not null,
+    car_id                bigint not null,
+    `pick-up_location_id` bigint not null,
     constraint `car_pick-up_location_pk`
-        primary key (id)
+        primary key (id),
+    foreign key (car_id) references car (id),
+    foreign key (`pick-up_location_id`) references `pick-up_location` (id)
 );
-alter table `car_pick-up_location`
-    add constraint `car_pick-up_location_car_fk`
-        foreign key (car) references car (id);
-alter table `car_pick-up_location`
-    add constraint `car_pick-up_location_location_fk`
-        foreign key (`pick-up_location`) references `pick-up_location` (id);
 
 
 
