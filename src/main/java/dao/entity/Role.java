604f27fc1d03a77ad8ee8b650e1ed312bdd01d94
kirillfursau <krill19988@gmail.com>
2020-05-28 23:04:04 +0300

    ADd Cache

--- src/main/java/dao/entity/Role.java
+++ src/main/java/dao/entity/Role.java
@@ -1,6 +1,8 @@
 package dao.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 
@@ -8,6 +10,8 @@ import javax.persistence.*;
 @Data
 @Entity
 @Table(name = "role")
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY, region = "my-cache")
 public class Role extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
